d9e8fcba2581a022b49f373d9874e6d11a03a81d
Rajat Poddar <rajatpoddar17@gmail.com>
2026-08-24 09:36:44 +0530

    chore: Bump versionCode to 4 and versionName to 1.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otprelay"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
